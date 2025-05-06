c5ecf23dde95a2552abee9542e837d70bb66a640
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-05-06 12:36:13 +0700

    menduplikasikan dan mengembalikan

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -26,5 +26,18 @@ class LinkedList
     
             Node *nodeBaru = new Node;
             nodeBaru->noMhs = nim;
+
+            if (START == NULL || nim <= START->noMhs)
+        {
+            if ((START != NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi nohs tidak diijinkan\n";
+                return;
+            }
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
+        
         }
 }
