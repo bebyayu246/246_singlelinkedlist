b6e1934ddf7afed4261d0533f273fe0c02ff3bd1
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-05-06 13:03:01 +0700

    membuat case 3 dan 4

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -155,6 +155,13 @@ int main()
                     cout << endl
                          << "Data dengan nomor mahasiswa " << nim << " berhasil dihapus" << endl;
             }
+            break;
+            case '3':
+            {
+                mhs.traverse();
+            }
+            break;
+            case '4':
                 
             
         }
