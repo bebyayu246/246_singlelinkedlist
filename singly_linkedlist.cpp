a23a9d3f5b7c4d82071ff163933b18aa0f9e4933
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-05-06 12:31:26 +0700

    membuat implementasi method addNode

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -17,4 +17,14 @@ class LinkedList
         {
             START = NULL;
         }
+
+        void addNode()
+        {
+            int nim;
+            cout << "\nMasukkan Nomor Mahasiswa: ";
+            cin >> nim;
+    
+            Node *nodeBaru = new Node;
+            nodeBaru->noMhs = nim;
+        }
 }
