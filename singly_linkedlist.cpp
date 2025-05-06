e3d9f48411610f464b9b27c7441ac16c7d7ff9f9
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-05-06 12:49:27 +0700

    mencari data dan perulangan data

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -60,4 +60,17 @@ class LinkedList
         {
             return (START == NULL);
         }
+        bool search(int nim, Node **previous, Node **current)
+    {
+        *previous = START;
+        *current = START;
+
+        while ((*current != NULL) && (nim != (*current)->noMhs))
+        {
+            *previous = *current;
+            *current = (*current)->next;
+        }
+
+        return (*current != NULL);
+    }
 };
